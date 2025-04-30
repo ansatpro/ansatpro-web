bcef45379d913dcd2a11e6a32c715407d47f31a2
JinshuZhang1201 <23773086@student.uwa.edu.au>
2025-04-30 17:22:16 +0800

    Update Facilitator Layout with Page-wide Comment
    - Added a comment in layout.js to clarify that the changes apply to all pages under the Facilitator folder.

--- src/app/facilitator/layout.js
+++ src/app/facilitator/layout.js
@@ -1,5 +1,5 @@
 "use client";
-
+// Apply to all pages under Facilitator folder
 import FacilitatorLayout from "@/layouts/FacilitatorLayout";
 
 export default function FacilitatorRootLayout({ children }) {
